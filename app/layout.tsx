c98bbf3e68cd4ec916033d24d324aa12f832d2ef
Ankit yadav <49826590+AnkitYadavnadaura@users.noreply.github.com>
2025-10-20 12:15:03 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import { MiniKitProvider } from "@worldcoin/minikit-js/minikit-provider";
 export const metadata = { title: 'P2P MiniApp' };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -9,12 +10,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 <script src="https://cdn.tailwindcss.com"></script>
 
       </head>
+      <MiniKitProvider>
       <body>
-        <header style={{padding:16, borderBottom:'1px solid #eee'}}>
-          <h1>P2P MiniApp (Vercel)</h1>
-        </header>
         <main style={{padding:16}}>{children}</main>
       </body>
+      </MiniKitProvider>
     </html>
   );
 }
