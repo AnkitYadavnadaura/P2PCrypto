d0f7c8de7f1c997c49d3523ca752fd05eee0fd41
Ankit yadav <49826590+AnkitYadavnadaura@users.noreply.github.com>
2025-10-20 12:21:14 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css';
-import { MiniKitProvider } from "@worldcoin/minikit-react";
+import { MiniKitProvider } from '@worldcoin/minikit-js';
 export const metadata = { title: 'P2P MiniApp' };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
