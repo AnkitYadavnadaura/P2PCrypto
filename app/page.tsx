e5b97840f4f8f82b8e961f3d14eadbdfac4d6ce5
Ankit yadav <49826590+AnkitYadavnadaura@users.noreply.github.com>
2025-10-20 09:41:32 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ const signInWithWallet = async () => {
 	// const { nonce } = await res.json()
 
 	const {commandPayload: generateMessageResult, finalPayload} = await MiniKit.commandsAsync.walletAuth({
-		nonce: 1,
+		nonce: Date.now().toString(),
 		requestId: '0', // Optional
 		expirationTime: new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000),
 		notBefore: new Date(new Date().getTime() - 24 * 60 * 60 * 1000),
