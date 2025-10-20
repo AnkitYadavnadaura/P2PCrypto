6a736e87affcad566790407e276065a802305f84
Ankit yadav <49826590+AnkitYadavnadaura@users.noreply.github.com>
2025-10-20 23:44:45 +0530

    Update route.ts

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,20 +1,30 @@
 import { NextResponse } from "next/server";
-import { verifyWalletAuth } from "@worldcoin/minikit-js";
+import { prisma } from "../../../lib/prisma";
+import { ethers } from "ethers";
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
 
     // Verify the payload from MiniKit
-    const isValid = await verifyWalletAuth(body.finalPayload);
+    // const isValid = await verifyWalletAuth(body.finalPayload);
 
-    if (!isValid) {
-      return NextResponse.json({ success: false, message: "Invalid wallet signature" }, { status: 401 });
-    }
+    // if (!isValid) {
+      // return NextResponse.json({ success: false, message: "Invalid wallet signature" }, { status: 401 });
+    // }
 
     // Extract wallet address
     const walletAddress = body.finalPayload.address;
+    const signature = body.finalPayload.signature;
+    const message = "This is my statement and here is a link https://worldcoin.com/apps";
+    if (!walletAddress || !signature) {
+      return NextResponse.json({ success: false, error: "Missing wallet data" }, { status: 400 });
+    }
+    const recovered = ethers.verifyMessage(message, signature);
 
+    if (recovered.toLowerCase() !== walletAddress.toLowerCase()) {
+      return NextResponse.json({ success: false, error: "Invalid signature" }, { status: 401 });
+    }
     // Save or find user in database (pseudo code)
     // const user = await db.user.upsert({ where: { wallet: walletAddress }, create: { wallet: walletAddress } });
     const user = await prisma.user.upsert({
