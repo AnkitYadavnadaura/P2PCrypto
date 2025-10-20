2794b56086b5c2da2889b441d9af256022f94999
Ankit yadav <49826590+AnkitYadavnadaura@users.noreply.github.com>
2025-10-20 17:47:20 +0530

    Create minikit-provider.tsx

---
+++ app/minikit-provider.tsx
@@ -0,0 +1,11 @@
+'use client';
+import {ReactNode , useEffect} from 'react'
+import {minikit} from '@worldcoin/minikit-js'
+
+export default function MiniKitProvider({children}:{children: ReactNode}){
+
+useEffect{()=>{
+  minikit.install()
+},[]}
+  return <>{children}</>
+}
