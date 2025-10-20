2794de2b61a74307630706234c851d30dcf43223
Ankit yadav <49826590+AnkitYadavnadaura@users.noreply.github.com>
2025-10-20 17:53:48 +0530

    Replace minikit with MiniKit in provider

--- app/minikit-provider.tsx
+++ app/minikit-provider.tsx
@@ -1,11 +1,11 @@
 'use client';
 import {ReactNode , useEffect} from 'react'
-import {minikit} from '@worldcoin/minikit-js'
+import {MiniKit} from '@worldcoin/minikit-js'
 
 export default function MiniKitProvider({children}:{children: ReactNode}){
 
 useEffect(()=>{
-  minikit.install()
+  MiniKit.install()
 },[])
   return <>{children}</>
 }
