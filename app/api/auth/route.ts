b42cd414cc9bfd7bcb4e49ba856fba7ef93b73d0
Ankit yadav <49826590+AnkitYadavnadaura@users.noreply.github.com>
2025-10-21 10:03:27 +0530

    Update route.ts

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -5,6 +5,7 @@ import { ethers } from "ethers";
 export async function POST(req: Request) {
   try {
     const body = await req.json();
+    return NextResponse.json({success:true,error:"something fucks"})
 
     // Verify the payload from MiniKit
     // const isValid = await verifyWalletAuth(body.finalPayload);
