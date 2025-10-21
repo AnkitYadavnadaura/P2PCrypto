2831cb7d42be144fee5d9bd71e8b08888e7bd2bc
Ankit yadav <49826590+AnkitYadavnadaura@users.noreply.github.com>
2025-10-21 22:41:14 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ const signInWithWallet = async () => {
       const res = await fetch("/api/auth", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(finalPayload),
+        body: JSON.stringify({finalPayload,message:generateMessageResult.siweMessage}),
       });
 
       const data = await res.json();
