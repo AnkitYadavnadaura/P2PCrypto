4e5af46cb1be489d8a724a2327163c8bdc4a73bf
Ankit yadav <49826590+AnkitYadavnadaura@users.noreply.github.com>
2025-10-21 23:31:05 +0530

    Update route.ts

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -17,7 +17,7 @@ export async function POST(req: Request) {
     const walletAddress = body.finalPayload.address;
     // return NextResponse.json({success:true,error:walletAddress})
     const signature = body.finalPayload.signature;
-    const message = str(body.message);
+    const message = String(body.message);
     // return NextResponse.json({success:true,error:"ethers fucks"})
     
     if (!walletAddress || !signature) {
