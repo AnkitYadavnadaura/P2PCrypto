96a848ea74887eb98faf4929f3cc277b5890b2d8
Ankit yadav <49826590+AnkitYadavnadaura@users.noreply.github.com>
2025-10-22 09:42:28 +0530

    Update route.ts

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,10 +1,26 @@
 import { NextResponse } from "next/server";
 import { prisma } from "../../../lib/prisma";
-import { SiweMessage } from "siwe";
+import { ethers } from "ethers";
+import {
+  MiniAppWalletAuthSuccessPayload,
+  verifySiweMessage,
+} from "@worldcoin/minikit-js";
+
+interface IRequestPayload {
+  payload: MiniAppWalletAuthSuccessPayload;
+  nonce: string;
+}
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
+    const { payload, nonce } = (await req.json()) as IRequestPayload;
+    const validMessage = await verifySiweMessage(payload, nonce);
+    return NextResponse.json({
+      status: "success",
+      isValid: validMessage.isValid,
+    });
+
 
     // Verify the payload from MiniKit
     // const isValid = await verifyWalletAuth(body.finalPayload);
@@ -17,20 +33,17 @@ export async function POST(req: Request) {
     const walletAddress = body.finalPayload.address;
     // return NextResponse.json({success:true,error:walletAddress})
     const signature = body.finalPayload.signature;
-     const message = new SiweMessage(body.message);
+    const message = body.finalPayload.message;
     // return NextResponse.json({success:true,error:"ethers fucks"})
     
     if (!walletAddress || !signature) {
       return NextResponse.json({ success: false, error: "Missing wallet data" }, { status: 400 });
     }
-    const result = await message.verify({
-      signature: body.finalPayload.signature,
-    });
-    return NextResponse.json(result)
+    const recovered = ethers.verifyMessage(message, signature);
 
-    // if (result.address.toLowerCase() !== walletAddress.toLowerCase()) {
-    //   return NextResponse.json({ success: false, error: "Invalid signature" ,recovered , walletAddress}, { status: 401 });
-    // }
+    if (recovered.toLowerCase() !== walletAddress.toLowerCase()) {
+      return NextResponse.json({ success: false, error: "Invalid signature" }, { status: 401 });
+    }
     // Save or find user in database (pseudo code)
     // const user = await db.user.upsert({ where: { wallet: walletAddress }, create: { wallet: walletAddress } });
     const user = await prisma.user.upsert({
