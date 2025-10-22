2c45b6759eeae1c423ef30ec93e2b7daab61e777
Ankit yadav <49826590+AnkitYadavnadaura@users.noreply.github.com>
2025-10-22 09:56:42 +0530

    Update route.ts

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -30,37 +30,37 @@ export async function POST(req: Request) {
     // }
 
     // Extract wallet address
-    const walletAddress = body.finalPayload.address;
+    // const walletAddress = body.finalPayload.address;
     // return NextResponse.json({success:true,error:walletAddress})
-    const signature = body.finalPayload.signature;
-    const message = body.finalPayload.message;
+    // const signature = body.finalPayload.signature;
+    // const message = body.finalPayload.message;
     // return NextResponse.json({success:true,error:"ethers fucks"})
     
-    if (!walletAddress || !signature) {
-      return NextResponse.json({ success: false, error: "Missing wallet data" }, { status: 400 });
-    }
-    const recovered = ethers.verifyMessage(message, signature);
+    // if (!walletAddress || !signature) {
+    //   return NextResponse.json({ success: false, error: "Missing wallet data" }, { status: 400 });
+    // }
+    // const recovered = ethers.verifyMessage(message, signature);
 
-    if (recovered.toLowerCase() !== walletAddress.toLowerCase()) {
-      return NextResponse.json({ success: false, error: "Invalid signature" }, { status: 401 });
-    }
+    // if (recovered.toLowerCase() !== walletAddress.toLowerCase()) {
+    //   return NextResponse.json({ success: false, error: "Invalid signature" }, { status: 401 });
+    // }
     // Save or find user in database (pseudo code)
     // const user = await db.user.upsert({ where: { wallet: walletAddress }, create: { wallet: walletAddress } });
-    const user = await prisma.user.upsert({
-      where: { walletAddress },
-      update: { createdAt: new Date() },
-      create: {
-        walletAddress,
-        email: null,
-        kycStatus: "PENDING",
-      },
-    });
+    // const user = await prisma.user.upsert({
+    //   where: { walletAddress },
+    //   update: { createdAt: new Date() },
+    //   create: {
+    //     walletAddress,
+    //     email: null,
+    //     kycStatus: "PENDING",
+    //   },
+    // });
 
-    return NextResponse.json({
-      success: true,
-      wallet: walletAddress,
-      user
-    });
+    // return NextResponse.json({
+    //   success: true,
+    //   wallet: walletAddress,
+    //   user
+    // });
   } catch (err) {
     console.error(err);
     return NextResponse.json({ success: false, error: err }, { status: 500 });
