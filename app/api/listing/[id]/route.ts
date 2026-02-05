3fca9425b8d522c9de8d175448fc94993079c890
Ankit yadav <49826590+AnkitYadavnadaura@users.noreply.github.com>
2026-02-05 17:40:45 +0530

    update

--- app/api/listing/[id]/route.ts
+++ app/api/listing/[id]/route.ts
@@ -19,6 +19,7 @@ export async function PATCH(
       balance,
       paymentMethods,
       maxTimeMinutes,
+      type,
       status,
     } = body;
 
@@ -26,7 +27,10 @@ export async function PATCH(
        AUTHORIZATION CHECK
        ========================= */
     const listing = await prisma.listing.findUnique({
-      where: { id: listingId },
+      where: { userid: walletAddress,
+        type:type,
+
+       },
     });
 
     if (!listing) {
@@ -59,7 +63,10 @@ export async function PATCH(
        UPDATE LISTING
        ========================= */
     const updated = await prisma.listing.update({
-      where: { id: listingId },
+      where: { userid: walletAddress,
+        type:type,
+
+       },
       data: {
         price: price ? new Prisma.Decimal(price) : undefined,
         minAmount: minAmount ? new Prisma.Decimal(minAmount) : undefined,
