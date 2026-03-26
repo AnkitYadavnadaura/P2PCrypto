4f4f03a63806708dfddd63d720ef94e7fa50a874
Ankit yadav <49826590+AnkitYadavnadaura@users.noreply.github.com>
2026-03-26 10:26:29 +0530

    Add ABI constants for listing functions

---
+++ app/lib/abi.ts
@@ -0,0 +1,6 @@
+export const ABI = [
+  "function createListing(uint256 amount, uint8 listingType)",
+  "function joinListing(uint256 id)",
+  "function markPaid(uint256 id)",
+  "function release(uint256 id)",
+];
