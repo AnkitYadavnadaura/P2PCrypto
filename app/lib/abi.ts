f637873a7345e14ed5fb88afe6688fdf4ed24a78
Ankit yadav <49826590+AnkitYadavnadaura@users.noreply.github.com>
2026-03-28 18:23:03 +0530

    Update abi.ts

--- app/lib/abi.ts
+++ app/lib/abi.ts
@@ -9,6 +9,6 @@ export const ABI = [
   "function listingCount() view returns (uint256)",
 
   // ⭐ OPTIONAL (future use)
-  "function listings(uint256 id) view returns (uint256 id, address seller, address buyer, uint256 amount, uint8 status, uint8 listingType)"
+  "function listings(uint256 id) view returns (uint256 id, address seller, address buyer, uint256 amount, uint8 status, uint8 listingType)",
   "function approve(address spender, uint256 amount)"
 ];
