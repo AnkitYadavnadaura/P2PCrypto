03366cb6f1398ea68f4aeb654bb5f86593990355
Ankit yadav <49826590+AnkitYadavnadaura@users.noreply.github.com>
2026-03-29 07:42:42 +0530

    Update contract address in contract.ts

--- app/lib/contract.ts
+++ app/lib/contract.ts
@@ -2,7 +2,7 @@ import { MiniKit } from "@worldcoin/minikit-js";
 import { ethers } from "ethers";
 import { ABI } from "./abi";
 
-const contractAddress = "0xb9650bc41b87dc0e6264fb47eacb78dd881dacbc";
+const contractAddress = "0x0560d82a7a8c34bf27974377447d7fbf7917c50d";
 const tokenAddress = "0x2cFc85d8E48F8EAB294be644d9E25C3030863003";
 
 const sendContractTx = async (
